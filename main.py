b282c6aae37b68a187efcfed588dcb5c127cf62f
Brent Young <bryoung777@icloud.com>
2023-02-02 15:31:58 -0800

    Attempt 1

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 def main():
     ##################################################
     # Comlete your code here
+    reg_hours = int(40)
+    reg_rate = float(18.25)
+    o_rate = float(27.78)
+    workhours = int(input("How many hours did you work this week?: "))
+    reg_charge = reg_hours * reg_rate
+    o_charge = (workhours - reg_hours) * o_rate
+    total_wage = reg_charge + o_charge
+    print(reg_charge)
+    print(o_charge)
+    print(total_wage)
     ##################################################
     pass
 
